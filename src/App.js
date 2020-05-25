57c6b67fde98d11c8623be0e93aa7cddb18ee0e4
rodrieri000 <ericarodriguez7@outlook.com>
2020-05-24 23:12:54 -0500

    Css updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Beer extends Component {
+  constructor(props)  {
+    super(props);
+    this.state = {
+      isLiked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
+  }
+
+  render() {
+    return (
+      <div className="body">        
+        <div className="beer">
+          <img style={{ width: "50px" }} src={this.props.beer.image_url} />
+          <h3>{this.props.beer.name}</h3>
+          <button onClick={this.handleClick}>{this.state.isLiked ? 'Liked' : 'Like'}</button>
+          <p>{this.props.beer.tagline}</p>
+          <div className="beer-description">{this.props.beer.description}</div>
+        </div>
+      </div>
+    )
+  }
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(json => json.json())
+    .then(data => {
+      this.setState({
+        beers: data
+      });
+      console.log(data);
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {}
+        {this.state.beers.map((beerData, index) => (
+          <Beer key={index} beer={beerData} />
+        ))}
+      </div>
+    );
+  }
+
+
+}
 export default App;
